2ec4c7ec90664b7c324675d40508140617b15c18
Kaung Chit Khine <kaung-chit-khine@cs-japan.co.jp>
2021-05-18 17:15:40 +0900

    1回目コミット

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/Header';
+import { BrowserRouter as Router } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <CssBaseline />
+      <Header />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root1')
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
