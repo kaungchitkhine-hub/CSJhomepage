763c34dc37e8fbb32ed22c167f9148da7e58f209
Kaung Chit Khine <kaung-chit-khine@cs-japan.co.jp>
2021-05-25 17:31:33 +0900

    20210525_プロジェクトにファイル分けて作成

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+import Rect from './Rect';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Rect />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,4 +17,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+export default Rect
